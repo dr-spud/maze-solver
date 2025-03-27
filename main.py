a1c54dff901e75de979f96873f1dadf1b8dadc2d
Neil <xzhuc42@gmail.com>
2025-03-28 00:12:47 +1100

    cells added

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from windowsetup import Window
+from windowsetup import Window, Point, Line, Cell
 
 def main():
     win = Window(800, 600)
+
+    c1 = Cell(100, 200, 100, 200, win, has_right_wall=False)
+
+    c1.draw()
+
+    c2 = Cell(400, 450, 400, 450, win, has_top_wall=False)
+
+    c2.draw()
+
+
+
+
+
     win.wait_for_close()
 
 main()
--- windowsetup.py
+++ windowsetup.py
@@ -1,5 +1,4 @@
 from tkinter import Tk, BOTH, Canvas
-
 class Window:
     def __init__(self, width, height):
         self.width = width
@@ -24,9 +23,69 @@ class Window:
             self.redraw()
 
         print(("window closed..."))
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
         
     def close(self):
         self.running = False
+
+    
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+    
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2
+        )
+
+class Cell:
+    def __init__(
+            self,
+            x1,
+            x2,
+            y1,
+            y2,
+            win,
+            has_left_wall=True,
+            has_right_wall=True,
+            has_top_wall=True,
+            has_bottom_wall=True  
+    ):
+        self.has_left_wall = has_left_wall
+        self.has_right_wall = has_right_wall
+        self.has_top_wall = has_top_wall
+        self.has_bottom_wall = has_bottom_wall
+        self.__x1 = x1
+        self.__x2 = x2
+        self.__y1 = y1
+        self.__y2 = y2
+        self.__win = win
+    
+    def draw(self):
+        walls = {
+            "left": (self.has_left_wall, Point(self.__x1, self.__y1), Point(self.__x1, self.__y2)),
+            "right": (self.has_right_wall, Point(self.__x2, self.__y1), Point(self.__x2, self.__y2)),
+            "top": (self.has_top_wall, Point(self.__x1, self.__y1), Point(self.__x2, self.__y1)),
+            "bottom": (self.has_bottom_wall, Point(self.__x1, self.__y2,), Point(self.__x2, self.__y2))
+        }
+
+        for wall_name, (has_wall, p1, p2) in walls.items():
+            if has_wall:
+                line = Line(p1, p2)
+                self.__win.draw_line(line, "black")
+
+
+
+
         
     
 
